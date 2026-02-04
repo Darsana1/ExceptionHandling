0828628ed1d5d60c1f7d056a9c15f16fc9138b34
DARSANA T <darsanaunni777@gmail.com>
2026-02-04 15:54:26 +0530

    first commit

---
+++ except1.py
@@ -0,0 +1,31 @@
+#1
+# num1=int(input("Enter first num: "))
+# num2=int(input('Enter second num: '))
+# try:
+#         print(num1/num2)
+# except ZeroDivisionError as z :
+#         print(z)
+
+
+#2
+# try:
+#         num=int(input('Enter num: '))
+# except ValueError:
+#         print('Invalid input.')
+# print("checking....")
+        
+#3
+# num=[9,89,56,2]
+# try:
+#         print(num[10])
+# except IndexError:
+#         print('List out of index range')
+
+#4
+
+# try:
+#         open('oops1.py')
+# except FileNotFoundError:
+#         print('File doesnot exist')
+
+# 5
